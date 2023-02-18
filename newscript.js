580c73a2e64357bc1222c9e1cf7780fa3a63b72c
Brinida <subedibrinita@gmail.com>
2023-02-18 12:52:39 +0545

    first commit

---
+++ newscript.js
@@ -0,0 +1,7 @@
+
+var typing=new Typed(".text", {
+    strings: ["", "Youtuber", "Freelancer", "Graphics Designer", "Web Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
